670533a8df5a5af03d07793a0e11f36ddfecd387
Kanojah <kanoa.hi@gmail.com>
2017-03-30 18:21:41 -1000

    done, try break me

--- darts.js
+++ darts.js
@@ -1,7 +1,22 @@
 /*jshint esversion: 6*/
 
 module.exports = (a) => {
+  var score = 0;
   if (Array.isArray(a) !== true) {
     return false;
   }
+  for (var i = 0; i < a.length; i++) {
+    if (typeof (a[i]) !== 'number') {
+      return false;
+    } else if (a[i] < 5 && a[i] !== 0) {
+      score += 10;
+    } else if (a[i] >= 5 && a[i] <= 10) {
+      score += 5;
+    }
+    score += 0;
+    if (score === (a.length * 10)) {
+      score += 100;
+    }
+  }
+  return score;
 };
--- tests/darts.spec.js
+++ tests/darts.spec.js
@@ -4,7 +4,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const darts = require('../darts.js');
-describe('darts', () => {
+describe('darts()', () => {
   it('should be a function', () => {
     expect(darts).to.be.a('function');
   });
@@ -14,5 +14,33 @@ describe('darts', () => {
     expect(darts({hey: 'hello'})).to.equal(false);
     expect(darts(null)).to.equal(false);
     expect(darts(undefined)).to.equal(false);
+  });
+  it('should only take an array of numbers', () => {
+    expect(darts([1, '17', undefined])).to.equal(false);
+  });
+  it('should only take in one set of scores at a time', () => {
+    expect(darts([12, 4, 1, 6], [14, 3, 12, 7])).to.equal(25);
+    expect(darts([12, 0, 3, 6], [1, 3, 2, 4, 2])).to.equal(15);
+  });
+  it('should add 10 points if hit radius is less than 5', () => {
+    expect(darts([1, 4, 12])).to.equal(20);
+  });
+  it('should add 5 points if hit radius is between 5 and 10, inclusive', () => {
+    expect(darts([1, 10, 8, 4, 5])).to.equal(35);
+  });
+  it('should add 0 points if hit radius is above 10 or the thrower misses', () => {
+    expect(darts([1, 10, 14, 13])).to.equal(15);
+    expect(darts([1, 10, 14, 6, 0, 2])).to.equal(30);
+  });
+  it('should add 100 bonus points if all hits have a radius less than 5', () => {
+    expect(darts([1, 3, 5, 0])).to.equal(25);
+    expect(darts([1, 1, 4, 2, 3, 2])).to.equal(160);
+  });
+  it('should return the throwers total score', () => {
+    expect(darts([1, 12, 8])).to.equal(15);
+    expect(darts([1, 12, 3, 7, 4])).to.equal(35);
+    expect(darts([1, 14, 9, 2, 0])).to.equal(25);
+    expect(darts([1, 4, 2, 2, 0, 3])).to.equal(50);
+    expect(darts([1, 4, 3, 2, 3])).to.equal(150);
   });
 });
